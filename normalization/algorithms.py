c0ad6ed9fe7bc44abd1c377d8e0fed5c9d66ab98
lulugtz11 <lulugutierrez32landa@gmail.com>
2025-04-22 00:09:00 -0600

    Update algorithms.py

--- normalization/algorithms.py
+++ normalization/algorithms.py
@@ -3,7 +3,21 @@ from .components import FunctionalDependency, Attribute, Relvar
 
 def closure(attributes: set[Attribute], functional_dependencies: set[FunctionalDependency]) -> set[Attribute]:
     # TODO: Actividad 3
-    raise NotImplementedError()
+    """
+    debemos regresar el cierre del conjunto de atributos i.e. conjunto de todos los atributos que pueden ser determinados funcionalmente
+    """
+    closure_set=set(attributes)  #hacemos copia del conjunto inicial
+    changed=True #variable para saber si el cierre cambió 
+
+    while changed: 
+        changed=False
+        for fd in functional_dependencies: #iteramos sobre las dependencias funcionales
+            #si el lado izquierdo está contenido en el cierre actual y hay atributos en el lado derecho que no están en el cierre
+            if fd.left.issubset(closure_set) and not fd.right.issubset(closure_set):
+                # Agregamos los atributos del lado derecho al cierre y seguimos iterando
+                closure_set.update(fd.right)
+                changed=True
+    return closure_set
 
 
 def is_superkey(attributes: set[Attribute], heading: set[Attribute], functional_dependencies: set[FunctionalDependency]) -> bool:
